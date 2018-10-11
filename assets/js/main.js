ca86fbbd6a6c835b16d70ee73b53eaa92030d2ef
Daniel Anner <daniel.anner@danstechsupport.com>
2018-10-11 12:14:27 -0400

    Finished making the ball move

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,23 @@
-var canvas = document.getElementById("main_canvas");
-var context = canvas.getContext("2d");
+var canvas = document.getElementById("main_canvas"); // Canvas, duh
+var context = canvas.getContext("2d"); // I know we could use ctx, but for the sake of easy readability spelt it out
 
-context.beginPath();
-context.rect(20, 40, 50, 50);
-context.fillStyle = "#FF0000";
-context.fill();
-context.closePath();
+var x = canvas.width/2;
+var y = canvas.height-30;
 
+var dx = 2;
+var dy = -2;
 
-context.beginPath();
-context.arc(240, 160, 20, 0, Math.PI*2, false);
-context.fillStyle = "green";
-context.fill();
-context.closePath();
+function drawBall() {
+  context.beginPath();
+  context.arc(x,y,10,0,Math.PI*2);
+  context.fillStyle = "#0095DD";
+  context.fill();
+  context.closePath();
+}
 
-context.beginPath();
-
-context.rect(160,10,100,40);
-context.strokeStyle = "rgba(0, 0, 255, 0.5)";
-context.stroke();
-context.closePath();
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+} setInterval(draw, 10);
